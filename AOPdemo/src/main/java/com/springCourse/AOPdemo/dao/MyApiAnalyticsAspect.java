d769fe8fbc0d9d09c1de9ed4e09cf75f4649fecb
Rubick <eithanllanosb@gmail.com>
2025-01-04 11:31:53 -0500

    Set orders for aspect files

--- AOPdemo/src/main/java/com/springCourse/AOPdemo/dao/MyApiAnalyticsAspect.java
+++ AOPdemo/src/main/java/com/springCourse/AOPdemo/dao/MyApiAnalyticsAspect.java
@@ -2,10 +2,12 @@ package com.springCourse.AOPdemo.dao;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(3)
 public class MyApiAnalyticsAspect {
     @Before("com.springCourse.AOPdemo.dao.AopExpressions.forDaoPackageNoGetterSetter()")
     public void performApiAnalytics() {
--- AOPdemo/src/main/java/com/springCourse/AOPdemo/dao/MyCloudLogAsyncAspect.java
+++ AOPdemo/src/main/java/com/springCourse/AOPdemo/dao/MyCloudLogAsyncAspect.java
@@ -2,10 +2,12 @@ package com.springCourse.AOPdemo.dao;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(1)
 public class MyCloudLogAsyncAspect {
     @Before("com.springCourse.AOPdemo.dao.AopExpressions.forDaoPackageNoGetterSetter()")
     public void logToCloudAsync() {
